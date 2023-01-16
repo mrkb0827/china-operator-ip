4d9c76d7d7e57380d01c9c5d017d991dc22d933a
mrkb0827 <95497028+mrkb0827@users.noreply.github.com>
2023-01-16 21:56:25 +0900

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -10,9 +10,10 @@ for file in operator/*.conf; do
 	log_info "generating IP list of $operator ..."
 	get_asn $file
 	get_asn $file | xargs bgptools -b rib.txt  | cidr-merger -s | grep -Fv : | cat > result/${operator}.txt  &
+	echo -e "payload:" > result/${operator}_clash.txt  &
+	cat result/${operator}.txt | sed '/./{s/^/  - IP-CIDR,&/}' >> result/${operator}_clash.txt  &
 	get_asn $file | xargs bgptools -b rib6.txt | grep -v '^::/0$' | cidr-merger -s | grep -F  : | cat > result/${operator}6.txt &
-	echo -e "payload:" >result/${operator}_clash.txt  &
-	cat result/${operator}.txt | sed '/./{s/^/  - IP-CIDR,&/}' >>result/${operator}_clash.txt  &
+
 done
 
 wait_exit
