743f227780aaa3a0f10b43acc6913ca7ceace8e5
mrkb0827 <95497028+mrkb0827@users.noreply.github.com>
2023-03-14 16:23:55 +0900

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -11,9 +11,10 @@ for file in operator/*.conf; do
 	get_asn $file
 	get_asn $file | xargs bgptools -b rib.txt  | cidr-merger -s | grep -Fv : | cat > result/${operator}.txt
 	echo -e "payload:" > result/${operator}_clash.txt
-	cat result/${operator}.txt | perl -ne '/(.+\/\d+)/ && print "  - |$1|\n"' | sed "s/|/'/g" >> result/${operator}_clash.txt
+	cat result/${operator}.txt | perl -ne '/(.+\/\d+)/ && print "  - |$1|\n"' | sed "s/|//g" >> result/${operator}_clash.txt
 	get_asn $file | xargs bgptools -b rib6.txt | grep -v '^::/0$' | cidr-merger -s | grep -F  : | cat > result/${operator}6.txt &
 
 done
-
+	echo -e "payload:" > result/china6_clash.txt
+	cat result/china6.txt | perl -ne '/(.+\/\d+)/ && print "  - |$1|\n"' | sed "s/|//g" >> result/china6_clash.txt
 wait_exit
