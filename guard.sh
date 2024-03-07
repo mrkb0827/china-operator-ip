bae7484d8e092a2bd1a48fe6d03590fb5d92babf
mrkb0827 <95497028+mrkb0827@users.noreply.github.com>
2024-03-08 00:58:15 +0900

    Update guard.sh

--- guard.sh
+++ guard.sh
@@ -2,6 +2,6 @@
 
 [[ $(wc -l result/lz_all_cn_cidr.txt) < 3000 ]] && exit 1
 
-[[ $(wc -l result/china6.txt) < 1000 ]] && exit 2
+[[ $(wc -l result/lz_all_cn_cidr6.txt) < 1000 ]] && exit 2
 
 exit 0
