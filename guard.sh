9a4552300305598c29f7f82d7d9e67f22f22e939
mrkb0827 <rin12_19@yahoo.co.jp>
2025-08-31 01:12:14 +0900

    Revert "mod"
    This reverts commit 0dcdd1a7a7e062614c3454d69b9d433e029c9318.

--- guard.sh
+++ guard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[[ $(wc -l result/lz_all_cn_cidr.txt) < 3000 ]] && exit 1
+[[ $(wc -l result/china.txt) < 3000 ]] && exit 1
 
-[[ $(wc -l result/lz_all_cn_cidr6.txt) < 1000 ]] && exit 2
+[[ $(wc -l result/china6.txt) < 1000 ]] && exit 2
 
 exit 0
